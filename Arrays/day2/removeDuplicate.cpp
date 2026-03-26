585234ea9b3c26674f22c0f74b90863c14f217a8
Surabhi Suhas Pednekar <surabhi@Surabhis-MacBook-Air.local>
2026-03-26 23:18:43 +0530

    day 2 done

--- Arrays/day2/removeDuplicate.cpp
+++ Arrays/day2/removeDuplicate.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeDuplicates(vector<int>& nums) {`
         vector<int>result;
         result.push_back(nums[0]);
         for(int i = 1;i<nums.size();i++){
