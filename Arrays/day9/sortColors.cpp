4f57271cc024e2ff757118e6e6d28c10923ec557
Surabhi Suhas Pednekar <surabhi@Surabhis-MacBook-Air.local>
2026-04-02 20:17:24 +0530

    day 9

---
+++ Arrays/day9/sortColors.cpp
@@ -0,0 +1,30 @@
+void sortColors(vector<int>& nums) {
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == 0){
+                countZero++;
+            }
+            else if(nums[i] == 1){
+                countOne++;
+            }
+            else{
+                countTwo++;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(countZero>0){
+                nums[i]=0;
+                countZero--;
+            }
+            else if(countOne>0){
+                nums[i]=1;
+                countOne--;
+            }else{
+                nums[i]=2;
+                countTwo--;
+            }
+        }
+        return ;
+    }
